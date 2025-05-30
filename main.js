649827caf720f32c5eed0b0b9534595ae740c206
Maria_Clara_Sampaio <127787008+MariaClaraSampaio25@users.noreply.github.com>
2025-05-30 08:04:39 -0300

    aula 3

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ console.log(botoes)
 
 for(let i=0; i< botoes.length; i++){
     botoes[i].onclick = function(){
+        
+        for (let j=0; j<botoes.length; j++){
+            botoes[j].remove.add("ativo");
+        }
         botoes[i].classList.add("ativo");
     }
 }
